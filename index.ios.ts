ab1782680d8fc17e8ec4c0ed2478c2a03c1e2498
Ricardo Bocchi <ricardo@mobilemind.com.br>
2020-10-15 18:55:52 -0300

    fix ns7

--- index.ios.ts
+++ index.ios.ts
@@ -1,11 +1,11 @@
 import { ios } from "utils/utils"; 
 
 export function getUUID() {
-    let mainBundle = ios.getter(NSBundle, NSBundle.mainBundle);
+    let mainBundle = NSBundle.mainBundle;
     let appName = mainBundle.infoDictionary.objectForKey(kCFBundleNameKey);
     var strApplicationUUID = SAMKeychain.passwordForServiceAccount(appName, "incoding");
     if (!strApplicationUUID) {
-        let currentDevice = ios.getter(UIDevice, UIDevice.currentDevice);
+        let currentDevice = UIDevice.currentDevice;
         strApplicationUUID = currentDevice.identifierForVendor.UUIDString;
         SAMKeychain.setPasswordForServiceAccount(strApplicationUUID, appName, "incoding");
     }
